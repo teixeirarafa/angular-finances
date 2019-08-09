7ad10f5011d267d6ab13a5ea3cc3f404ef80d829
Rafael Teixeira Da Silva <rtsilva@brq.com>
2019-08-09 18:34:55 -0300

    improving category list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { InMemoryDatabase } from "./in-memory-database";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   providers: [],
--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CategoryService } from "./../shared/category.service";
+import { Category } from "./../shared/category.model";
 
 @Component({
-  selector: 'app-category-list',
-  templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  selector: "app-category-list",
+  templateUrl: "./category-list.component.html",
+  styleUrls: ["./category-list.component.css"]
 })
 export class CategoryListComponent implements OnInit {
+  categories: Category[] = [];
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit() {
+    this.categoryService
+      .getAll()
+      .subscribe(categories => (this.categories = categories));
   }
 
+  deleteCategory(category: Category) {
+    this.categoryService
+      .delete(category.id)
+      .subscribe(
+        () =>
+          (this.categories = this.categories.filter(item => item != category))
+      );
+  }
 }
